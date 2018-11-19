96c41b528d03ab0c42259298bdeda7afbb79edfa
Jason Ho <jasoryeh@gmail.com>
2018-11-18 21:33:22 -0800

    add some info stuff

--- src/main/java/net/vectormc/conductor/ConductorMain.java
+++ src/main/java/net/vectormc/conductor/ConductorMain.java
@@ -4,11 +4,19 @@ import net.vectormc.conductor.downloaders.Downloader;
 import net.vectormc.conductor.log.Logger;
 
 import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
 
 public class ConductorMain {
 
     public static void main(String[] args) {
         Logger.getLogger().info("--- Info ---");
+        RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+        List<String> arguments = runtimeMXBean.getInputArguments();
+        String argumentFull = String.join(" ", arguments);
+        Logger.getLogger().info("Arguments - " + argumentFull);
+
         Logger.getLogger().info("File Test - " + new File("test").getAbsolutePath());
 
         Logger.getLogger().info("File separator - " + File.separator);
