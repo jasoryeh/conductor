bf855d237e102e0dbb69e8afb1f89e1829b26f7a
Jason <jasoryeh@gmail.com>
2021-06-28 12:39:20 -0700

    Remove main in variable resolver

--- src/main/java/tk/jasoryeh/conductor/processor/VariableResolver.java
+++ src/main/java/tk/jasoryeh/conductor/processor/VariableResolver.java
@@ -1,6 +1,5 @@
 package tk.jasoryeh.conductor.processor;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -23,32 +22,18 @@ public class VariableResolver {
                 String key = var.getKey();
                 String value = var.getValue();
 
-                // regular
-                temp = temp.replaceAll(Pattern.quote("{{" + key + "}}"), value);
-                // strict
-                temp = temp.replaceAll(Pattern.quote("{{!" + key + "!}}"), value);
+                temp = temp.replaceAll(Pattern.quote("{{" + key + "}}"), value); // regular
+                temp = temp.replaceAll(Pattern.quote("{{!" + key + "!}}"), value); // strict
             }
             for (Map.Entry<String, String> env : System.getenv().entrySet()) {
                 String key = env.getKey();
                 String value = env.getValue();
 
-                // regular
-                temp = temp.replaceAll(Pattern.quote("{{" + key + "}}"), value);
-                // env
-                temp = temp.replaceAll(Pattern.quote("{{$" + key + "$}}"), value);
+                temp = temp.replaceAll(Pattern.quote("{{" + key + "}}"), value); // regular
+                temp = temp.replaceAll(Pattern.quote("{{$" + key + "$}}"), value); // env
             }
         }
         return temp;
-    }
-
-    public static void main(String[] args) {
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
-        stringStringHashMap.put("ASDF", "OT WORKS!!! WOOT WOOT ///// \\ asdf");
-        stringStringHashMap.put("STATIC_AUTH", "input:testasdfasdf");
-
-        String s = resolveVariables(stringStringHashMap, "{{ASD_SHOULDSKIPTHIS}} {{STATIC_AUTH}}@but this should work {{ASDF}}, but i think this is an env {{JAVA_HOME}}", true).replace("\\\\", "/");
-        System.out.println(System.getenv().toString().replaceAll(", ", ", " + System.lineSeparator()));
-        System.out.println(s);
     }
 
 }
