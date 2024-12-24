3d2f9a382bb6a04b187d6e6318ecf28584baf2d8
Jason Ho <git@hogt.me>
2024-12-24 12:54:57 -0800

    document Plugin.java more

--- src/main/java/tk/jasoryeh/conductor/plugins/Plugin.java
+++ src/main/java/tk/jasoryeh/conductor/plugins/Plugin.java
@@ -1,9 +1,14 @@
 package tk.jasoryeh.conductor.plugins;
 
 import lombok.Getter;
+import tk.jasoryeh.conductor.V2FileObject;
 import tk.jasoryeh.conductor.V2FileSystemObject;
 import tk.jasoryeh.conductor.log.Logger;
 
+/**
+ * @see V2FileObject#getFile()
+ * @see V2FileObject#getTemporary()
+ */
 public abstract class Plugin {
 
     @Getter
@@ -18,11 +23,24 @@ public abstract class Plugin {
 
     /**
      * Prepare required resources in the temporary directory.
+     *
+     * Operations occur first in prepare() first so that resources in the actual
+     * directory are not affected should a failure occur.
+     *
+     * - Typically operations like downloading from a remote server,
+     *      or any additional calculations or substitutions are done here first.
+     *
+     * @see V2FileSystemObject#getTemporary()
      */
     public abstract void prepare();
 
     /**
      * Perform changes to the final destination.
+     *
+     * - Typically to copy the prepared resource from temporary directory
+     *      or memory to the final file location.
+     *
+     * @see V2FileSystemObject#getFile()
      */
     public abstract void execute();
 
